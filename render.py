f53e49301b5b19aeace081db8f9899db33249713
Cristian Sirbu <cmsirbu@users.noreply.github.com>
2024-07-17 18:19:16 +0100

    Get render pipeline going

--- render.py
+++ render.py
@@ -2,6 +2,8 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from ruamel.yaml import YAML
 from rich import print
 
+import os
+
 yaml = YAML(typ="safe")
 with open("data.yaml") as data_file:
     data = yaml.load(data_file.read())
@@ -12,4 +14,16 @@ env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoes
 
 template = env.get_template("index.html.j2")
 
-print(template.render(data))
+output_path = "output"
+
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
+
+with open(
+    os.path.join(
+        output_path,
+        "index.html",
+    ),
+    "w",
+) as output_file:
+    output_file.write(template.render(data))
