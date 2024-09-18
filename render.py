0092c902807b2700b0edd2e1a6d511c83c0913c1
Cristian Sirbu <cmsirbu@users.noreply.github.com>
2024-09-18 22:36:21 +0100

    WIP data from PyPI

--- render.py
+++ render.py
@@ -1,7 +1,8 @@
 """Generates status pages for the Nautobot Ecosystem."""
 
 import os
-from datetime import datetime, timezone
+import re
+from datetime import datetime, timezone, timedelta
 
 import requests
 import requests_cache
@@ -17,6 +18,18 @@ def _get_yaml_data(filename):
         return yaml.load(data_file.read())
 
 
+def _get_pypi_api_response(project):
+    """Return data from PyPI JSON API."""
+    url = f"https://pypi.python.org/pypi/{project}/json"
+    headers = {"Accept": "application/json"}
+    resp = requests.get(url, headers=headers, timeout=10)
+
+    if resp.ok:
+        return resp.json()
+    else:
+        return None
+
+
 def _get_github_api_response(url, token=None):
     """Return data from GitHub JSON API."""
     headers = {"Accept": "application/vnd.github+json", "X-GitHub-Api-Version": "2022-11-28"}
@@ -28,6 +41,19 @@ def _get_github_api_response(url, token=None):
         return resp.json()
     else:
         return None
+
+
+def _generate_page(page, **kwargs):
+    """Generates a new file in memory from a Jinja template."""
+    with open(
+        os.path.join(
+            OUTPUT_PATH,
+            page,
+        ),
+        "w",
+    ) as output_file:
+        template = JINJA_ENV.get_template(f"{page}.jinja")
+        output_file.write(template.render(**kwargs))
 
 
 def _get_run_streak(runs, target_status, opposite_status):
@@ -75,18 +101,34 @@ def get_github_upstream_testing_results(project):  # noqa: PLR0912 too-many-bran
     #     )
 
 
-def _generate_page(page, **kwargs):
-    """Generates a new file in memory from a Jinja template."""
-    with open(
-        os.path.join(
-            OUTPUT_PATH,
-            page,
-        ),
-        "w",
-    ) as output_file:
-        template = JINJA_ENV.get_template(f"{page}.jinja")
-        output_file.write(template.render(**kwargs))
+def get_pypi_data(project):
+    """Fetch and parse upstream testing workflow results."""
+    pypi_raw_data = _get_pypi_api_response(project["pypi"])
+    pypi_data = {
+        "latest": {
+            "version": pypi_raw_data["info"]["version"],
+            "requires_python": pypi_raw_data["info"]["requires_python"],
+            "requires_nautobot": "N/A",
+            "date": pypi_raw_data["urls"][0]["upload_time"].split("T")[0],
+        }
+    }
 
+    release_date = datetime.fromisoformat(pypi_raw_data["urls"][0]["upload_time"])
+    if datetime.now() - release_date > timedelta(weeks=12):
+        pypi_data["latest"]["date_color"] = "danger"
+    elif datetime.now() - release_date > timedelta(weeks=6):
+        pypi_data["latest"]["date_color"] = "warning"
+    else:
+        pypi_data["latest"]["date_color"] = "success"
+
+    r = re.compile("nautobot[<> ]")
+    for result in filter(r.match, pypi_raw_data["info"]["requires_dist"]):
+        pypi_data["latest"]["requires_nautobot"] = result
+
+    return pypi_data
+
+
+### Main Script
 
 PROJECTS = _get_yaml_data("data.yaml")
 JINJA_ENV = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoescape())
@@ -98,15 +140,17 @@ requests_cache.install_cache(REQUESTS_CACHE_PATH, backend="filesystem", expire_a
 
 if __name__ == "__main__":
     upstream_data = {}
+    pypi_data = {}
     for project in PROJECTS["nautobot"]:
         upstream_data[project["repo"]] = get_github_upstream_testing_results(project)
+        pypi_data[project["repo"]] = get_pypi_data(project)
 
     if not os.path.exists(OUTPUT_PATH):
         os.makedirs(OUTPUT_PATH)
 
     build_timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S %Z")
 
-    _generate_page("index.html", projects=PROJECTS)
+    _generate_page("index.html", projects=PROJECTS, pypi=pypi_data, build_timestamp=build_timestamp)
     _generate_page("badges.html", projects=PROJECTS)
     _generate_page(
         "upstream.html", projects=PROJECTS, upstream=upstream_data, build_timestamp=build_timestamp
